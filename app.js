a46dfbd245cf09dbd41b7a564b73a3d5c3d8d4ec
Mewen8 <aziliz.roche63@hotmail.com>
2026-06-18 23:30:27 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+getFirestore,
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyBYgyM1epQ9up8195FarX7gBnli1LQs2_U",
+authDomain: "activites-anniversaire.firebaseapp.com",
+projectId: "activites-anniversaire",
+storageBucket: "activites-anniversaire.firebasestorage.app",
+messagingSenderId: "613450740235",
+appId: "1:613450740235:web:c8c0d071afa6a045598d0e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const activitiesDiv = document.getElementById("activities");
+const statusDiv = document.getElementById("status");
+
+async function loadActivities() {
+activitiesDiv.innerHTML = "";
+
+const snapshot = await getDocs(collection(db, "activities"));
+
+snapshot.forEach((doc) => {
+const data = doc.data();
+
+```
+const div = document.createElement("div");
+div.className = "activity";
+
+div.innerHTML = `
+  <label>
+    <input type="radio" name="activity" value="${doc.id}">
+    ${data.name}
+  </label>
+`;
+
+activitiesDiv.appendChild(div);
+```
+
+});
+
+statusDiv.textContent = "Choisis une activité puis vote.";
+}
+
+loadActivities();
