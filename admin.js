f16a6e8f1996ad74762b567f2c3b32dcd400c2af
Mewen8 <aziliz.roche63@hotmail.com>
2026-06-20 19:44:28 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,40 +1,47 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
-getFirestore,
-collection,
-getDocs
+  getFirestore,
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBYgyM1epQ9up8195FarX7gBnli1LQs2_U",
-authDomain: "activites-anniversaire.firebaseapp.com",
-projectId: "activites-anniversaire",
-storageBucket: "activites-anniversaire.firebasestorage.app",
-messagingSenderId: "613450740235",
-appId: "1:613450740235:web:c8c0d071afa6a045598d0e"
+  apiKey: "AIzaSyBYgyM1epQ9up8195FarX7gBnli1LQs2_U",
+  authDomain: "activites-anniversaire.firebaseapp.com",
+  projectId: "activites-anniversaire",
+  storageBucket: "activites-anniversaire.firebasestorage.app",
+  messagingSenderId: "613450740235",
+  appId: "1:613450740235:web:c8c0d071afa6a045598d0e"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const activityList = document.getElementById("activityList");
+document
+  .getElementById("startVote")
+  .addEventListener("click", async () => {
 
-async function loadActivities() {
-activityList.innerHTML = "";
+    await updateDoc(
+      doc(db, "config", "vote"),
+      {
+        active: true
+      }
+    );
 
-const snapshot = await getDocs(collection(db, "activities"));
-
-snapshot.forEach((doc) => {
-const div = document.createElement("div");
-
-```
-div.className = "activity";
-div.textContent = doc.data().name;
-
-activityList.appendChild(div);
-```
-
+    alert("Vote lancé !");
 });
-}
 
-loadActivities();
+document
+  .getElementById("endVote")
+  .addEventListener("click", async () => {
+
+    await updateDoc(
+      doc(db, "config", "vote"),
+      {
+        active: false
+      }
+    );
+
+    alert("Vote terminé !");
+});
